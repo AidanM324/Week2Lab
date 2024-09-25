aaaefbe76cc1527b921741d48689d4fadff2e04b
Week2Lab <G00423306@atu.ie>
2024-09-25 15:58:29 +0100

    Created Calculator method

--- src/main/java/ie/atu/week2/RequestController.java
+++ src/main/java/ie/atu/week2/RequestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/person")
 public class RequestController {
+
     @GetMapping("/sayHello")
     public String sayHello()
     {
@@ -22,4 +23,35 @@ public class RequestController {
     {
         return name + " " + age;
     }
+
+    @GetMapping("/calculate")
+    public String calculate(@RequestParam int num1, @RequestParam int num2, @RequestParam String operation)
+    {
+        float total =0;
+
+        switch (operation) {
+            case "add":
+                total = num1 + num2;
+                break;
+            case "subtract":
+                total = num1 - num2;
+                break;
+            case "multiply":
+                total = num1 * num2;
+                break;
+            case "divide":
+                if(num2 == 0){
+                    return "cant divide by 0";
+                }
+                else{
+                    total = (float)num1 / num2;
+                }
+                break;
+            default:
+                break;
+        }
+
+        return Float.toString(total);
+    }
+
 }
